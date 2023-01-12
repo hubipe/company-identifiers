5e8a4f893e7e7ba50f435a9d9adec3455203b032
Petr Hubík <petr@hubik.net>
2023-01-12 10:00:49 +0100

    Austria: company ID checksum letter check

--- src/CountryValidators/Austria.php
+++ src/CountryValidators/Austria.php
@@ -7,20 +7,36 @@ use Consistence\ObjectPrototype;
 class Austria extends ObjectPrototype implements CountryValidator
 {
 
+	private const COMPANY_ID_WEIGHTS = [6, 4, 14, 15, 10, 1];
+	private const COMPANY_ID_CHECK_CHARACTERS = 'abdfghikmpstvwxyz';
+
 	use EuropeanVatFormatter;
 
 	public function isCompanyIdValid(string $id): bool
 	{
-		return preg_match('/^[Ff][Nn]\s*[1-9][0-9]{4,5}\s*[a-zA-Z]$/', trim($id)) === 1;
+		$matches = [];
+		if (!preg_match('/^(FN\s*)?(?P<num>[1-9][0-9]{4,5})\s*(?P<char>['. self::COMPANY_ID_CHECK_CHARACTERS .'])$/i', trim($id), $matches)) {
+			return FALSE;
+		}
+
+		$num = str_pad($matches['num'], 6, '0', STR_PAD_LEFT);
+		$char = strtolower($matches['char']);
+
+		$sum = 0;
+		for ($i = 0; $i < 6; $i++) {
+			$sum += (int)$num[$i] * self::COMPANY_ID_WEIGHTS[$i];
+		}
+		$checkCharacter = self::COMPANY_ID_CHECK_CHARACTERS[$sum % 17];
+		return $checkCharacter === strtolower($char);
 	}
 
 	public function formatCompanyId(string $id): string
 	{
 		$matches = [];
-		if (!preg_match('/^[Ff][Nn]\s*(?P<num>[1-9][0-9]{4,5})\s*(?P<letter>[a-zA-Z])$/', $id, $matches)) {
+		if (!preg_match('/^(FN\s*)?(?P<num>[1-9][0-9]{4,5})\s*(?P<letter>['. self::COMPANY_ID_CHECK_CHARACTERS .'])$/i', $id, $matches)) {
 			return $id;
 		}
-		return 'FN ' . $matches['num'] . ' ' . strtolower($matches['letter']);
+		return $matches['num'] . strtolower($matches['letter']);
 	}
 
 	public function getCompanyIdentifier(string $id): string
